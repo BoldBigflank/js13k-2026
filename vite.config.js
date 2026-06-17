ec84b4d5fe2fd13d02cdcd37b6d08e82b7a5deb3
Alex Swan <alex@bold-it.com>
2026-06-17 12:23:07 -0500

    initial

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { viteExternalsPlugin } from "vite-plugin-externals";
+
+export default {
+  base: "",
+  server: {
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "0.0.0.0",
+      "ada3d69471bb.ngrok-free.app",
+      "together-horribly-lamprey.ngrok-free.app",
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
+};
